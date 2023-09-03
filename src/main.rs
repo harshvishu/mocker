09d86efa4c1783a7dd124f42863cdc534531873e
Harsh <vishwakarma.harsh1993@gmail.com>
2023-09-03 13:46:59 +0530

    added default service basic implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use actix_web::middleware::{Compress, Logger, NormalizePath};
+use actix_web::web::Data;
 use actix_web::{Error, HttpRequest};
 use serde::Deserialize;
 use serde_json::{json, Value};
@@ -20,28 +22,10 @@ async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
 
-#[post("/.*")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
 async fn get_http_response(req: HttpRequest) -> impl Responder {
     println!("{:#?}", req);
     println!("{}", req.path());
     HttpResponse::Ok().body("response")
-}
-
-async fn manual_reply() -> impl Responder {
-    let body = web::Bytes::from_static(b"");
-
-    let json_raw = r##"{name: "Harsh"}"##;
-
-    HttpResponse::Ok()
-        .status(StatusCode::FORBIDDEN)
-        //.content_type("application/json")
-        .content_type(ContentType::json())
-        //.json(r"{name:"Harsh"}")
-        .body(json_raw)
 }
 
 fn configure_routes(config: &mut web::ServiceConfig) {
@@ -78,18 +62,40 @@ fn configure_routes(config: &mut web::ServiceConfig) {
     }
 }
 
+async fn handle_default_service(req: HttpRequest, state: Data<AppState>) -> impl Responder {
+    // println!("{:#?}", req);
+    println!("{}", req.path());
+    println!("{:#?}", state.cache);
+    let mut path = req.path();
+    if path.starts_with('/') {
+        path = &path[1..];
+    }
+    match &state.cache.get(path) {
+        Some(response) => {
+            let response = &(*response).clone();
+            HttpResponse::Ok().body(response.clone())
+        }
+        None => HttpResponse::NotFound().body(format!("response for path: '{}' not found", path)),
+    }
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let app_data = web::Data::new(AppState {
-        cache: HashMap::new(),
+    let app_data = Data::new(AppState {
+        cache: create_file_map(),
     });
+
+    println!("{:#?}", app_data.cache);
 
     HttpServer::new(move || {
         App::new()
+            .wrap(Compress::default())
+            .wrap(Logger::default())
+            .wrap(NormalizePath::default())
             .app_data(app_data.clone())
             .service(hello)
-            .service(echo)
-            .configure(configure_routes)
+            .default_service(web::to(handle_default_service))
+        //.configure(configure_routes)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
@@ -100,6 +106,38 @@ fn read_json_file(file: File) -> Result<Request, Box<dyn std::error::Error>> {
     let reader = BufReader::new(file);
     let request = serde_json::from_reader(reader)?;
     Ok(request)
+}
+
+fn create_file_map() -> HashMap<String, String> {
+    let mut map = HashMap::new();
+
+    let paths: Vec<PathBuf> = fs::read_dir("./")
+        .unwrap()
+        .filter_map(|dir| dir.ok())
+        .map(|dir_entry| dir_entry.path())
+        .filter_map(|path| {
+            if path.extension().map_or(false, |ext| ext == "json") {
+                Some(path)
+            } else {
+                None
+            }
+        })
+        .collect();
+
+    for path in paths {
+        if let Ok(file) = File::open(path.clone()) {
+            match read_json_file(file) {
+                Ok(result) => {
+                    let url = result.url.clone();
+                    let path = path.file_name().unwrap().to_str().unwrap().to_string();
+                    let response = serde_json::to_string(&result.response).unwrap();
+                    map.insert(url, response);
+                }
+                Err(err) => println!("{}", err),
+            }
+        }
+    }
+    map
 }
 
 #[derive(Debug, Deserialize, Clone)]
@@ -114,4 +152,10 @@ struct Request {
 
 struct AppState {
     cache: HashMap<String, String>,
+}
+
+impl AppState {
+    fn new(cache: HashMap<String, String>) -> Self {
+        Self { cache }
+    }
 }
